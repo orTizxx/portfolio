3f7a3ea1b8e3cdf1ab87a2b14fe6dd8fb1549245
orTizxx <nasiruddinns08@gmail.com>
2024-11-21 23:41:32 +0530

    added speed insight

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { MyPortfolio } from "./components/main/MyPortfolio";
 
 const App = () => {
   return (
     <div>
+      <SpeedInsights />
       <MyPortfolio />
     </div>
   );
